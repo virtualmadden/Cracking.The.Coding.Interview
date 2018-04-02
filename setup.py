f5ff8743bdfa49260944e1b9812f9b232424491b
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-04-02 12:54:26 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cracking.The.Coding.Interview',
+      version='0.0.1',
+      description='Cracking the Coding Interview problems implemented in Python.',
+      author='Jonathan Madden',
+      )
